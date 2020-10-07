06da1a136221241bf8327dc2ddcc6b489fc55b81
bibekrawat2020 <rawatbibek0@gmail.com>
2020-10-07 18:31:58 +0545

    New Program Added

---
+++ me.py
@@ -0,0 +1,9 @@
+#About Me
+name = input('Enter your name :')
+age = int(input('Enter your age :'))
+add = input('Enter your address :')
+dob = (input('Enter your date of birth :'))
+print('Your name is ' + name)
+print('Your age is ' + f'{age}')
+print('Your address is ' + add)
+print('Your date of birth is ' + dob)
