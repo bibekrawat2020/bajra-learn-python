a1876bc65c7f6e8c9bc9e8cd8a82c691cb206ad4
bibekrawat2020 <rawatbibek0@gmail.com>
2020-10-08 15:11:50 +0545

    if_else basics

--- if_else.py
+++ if_else.py
@@ -1,8 +1,23 @@
-a = 5
-b = 5
-if b>a:
-    print('b is greater than a')
-elif b==a:
-    print('b is equal to a')
-else:
-    print('b is smaller than a')
+# a = 5
+# b = 5
+# if b>a:
+#     print('b is greater than a')
+# elif b==a:
+#     print('b is equal to a')
+# else:
+#     print('b is smaller than a')
+
+# num = int(input('Enter a number :'))
+# res = print(f'{num}' + " is greater than 10") if num > 10 else print(f'{num}' + " is smaller than 10")
+
+# grade = 90
+# if grade > 40:
+#     if grade >= 60:
+#         if grade < 80:
+#             print('First division')
+#     elif grade > 80 and grade < 90:
+#         print('Distinction')
+#     else:
+#         print('Second division')
+# else:
+#     print('fail')
