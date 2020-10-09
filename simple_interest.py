634a0f395a33e99dcd214be9517ee1690f43b0ce
bibekrawat2020 <rawatbibek0@gmail.com>
2020-10-09 07:08:54 +0545

    add SI

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+#Input
+prin = float(input("Enter principal :"))
+time = float(input("Enter time :"))
+rate = float(input("Enter rate :"))
+
+#Operation
+SI = (prin * time * rate) / 100 
+
+#Result 
+print("Simple interest is ", SI)
